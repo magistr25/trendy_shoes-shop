9eb75a57fc1de6d4c1bbce03320f58b603c1791c
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-06-21 13:29:48 +0600

    Поправила стили

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 import { Card } from '../components/Card';
+import "../index.scss"
+
 
 export const Home = ({ items, searchValue, onChangeSearchInput, onAddToFavorite, onRemoveFavorite, onAddToCart, isItemAdded, isItemFavorite }) => {
     const filteredItems = items.filter(item => item.title.toLowerCase().includes(searchValue.toLowerCase()));
 
     return (
-        <div className= "content p-40">
-            <div className="d-flex justify-between align-center p-40 ">
+        <div className= "content p-40 ">
+            <div className="d-flex justify-between align-center ">
                 <img className="br-20" src={`${process.env.PUBLIC_URL}img/banner.png`}
                      alt="" width={1000} height={500}/>
             </div>
-            <div className="d-flex justify-between align-center p-40">
-                <h1>{searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все кроссовки'}</h1>
-                <div className="search-block d-flex">
+            <div className="d-flex  justify-between align-center p-40">
+                <h1 className="ml-20"> {searchValue ? `Поиск по запросу: "${searchValue}"` : 'Все кроссовки'}</h1>
+                <div className="search-block">
                     <img src={`${process.env.PUBLIC_URL}/img/search.svg`} alt="Search"/>
                     <input onChange={onChangeSearchInput} value={searchValue} placeholder="Поиск..."/>
                 </div>
             </div>
-            <div className="d-flex flex-wrap">
+            <div className="d-flex flex-wrap space-between">
                 {filteredItems.map((item) => (
                     <Card
                         key={item.itemId}
